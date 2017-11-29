fcb55c831e2ae3d34106e5baab51f7a8703294e1
Reshetnikov Artem <mr.artem.reshetnikov.2017@mail.ru>
2017-11-29 16:42:06 +0500

    7

--- async.js
+++ async.js
@@ -3,15 +3,29 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
+/** Функция преобразует job в объект
+ * @param {Array} job – функция, которая возвращает промис
+ * @param {Number} jobIndex - индекс функции в массиве jobs
+ * @returns {Object}
+ */
 function jobToObject(job, jobIndex) {
     return {
         getPromise: job,
         index: jobIndex,
         startTime: 0,
-        endTime: 0,
-        result: []
+        endTime: 0
     };
+}
 
+/** Если promise выполняется дольше timeout то reject
+ * @param {Number} timeout - время, дольше которого не должен исполняться promise
+ * @returns {Object}
+ */
+function limitByTime(timeout) {
+    return job => () => new Promise((resolve, reject) => {
+        job().then(resolve, reject);
+        setTimeout(() => reject(new Error('Promise timeout')), timeout);
+    });
 }
 
 /** Функция паралелльно запускает указанное число промисов
@@ -21,9 +35,13 @@ function jobToObject(job, jobIndex) {
  * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    let jobsObjects = jobs.map((job, jobIndex) => jobToObject(job, jobIndex));
+    let jobsObjects = jobs.map(limitByTime(timeout))
+        .map((job, jobIndex) => jobToObject(job, jobIndex));
     let finished = 0;
 
+    /** Функция возвращает массив result'ов из всех jobsObject
+     * @returns {Array}
+     */
     function getResults() {
         let results = [];
         jobsObjects.forEach((jobObject) => results.push(jobObject.result));
@@ -31,19 +49,28 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
         return results;
     }
 
+    /** 
+     * @param {Array} resolve 
+     * @param {Number} result 
+     * @param {Number} jobObject
+     */
     function onResult(resolve, result, jobObject) {
         jobObject.result = result;
         ++finished;
         if (jobs.length === finished) {
             resolve(getResults());
-        } else if (jobsObjects.length > 0) {
+        } else if (jobsObjects.length) {
             runPromise(resolve, jobsObjects.shift());
         }
     }
 
+    /**
+     * @param {Array} resolve
+     * @param {Number} jobObject
+     */
     function runPromise(resolve, jobObject) {
         let handler = result => onResult(resolve, result, jobObject);
-        jobObject.getPromise().then(handler, handler);
+        jobObject.getPromise().then(handler);
     }
 
     function runPromises() {
@@ -60,3 +87,4 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
 
     return runPromises();
 }
+
