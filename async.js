cbbdf6f92a8761ffae0d2f17e6a3fca1eaa61c60
Reshetnikov Artem <mr.artem.reshetnikov.2017@mail.ru>
2017-11-29 13:01:52 +0500

    1

--- async.js
+++ async.js
@@ -6,8 +6,24 @@ exports.runParallel = runParallel;
 /** Функция паралелльно запускает указанное число промисов
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
+ * @param {Number} timeout - таймаут работы 
+ * @returns {Promise}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+function runParallel(jobs, parallelNum) {
+    let result = [];
+
+    function getPromise() {
+        return new Promise(resolve => {
+            if (parallelNum > 0 && jobs.length > 0) {
+                jobs.forEach((job) => {
+                    let state = job => resolve(result);
+                    job().then(state, state);
+                });
+            } else {
+                resolve(result);
+            }
+        });
+    }
+
+    return getPromise();
 }
